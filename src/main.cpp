fbc8ba8795e521d4f473e6b21d21ca1551497c04
Harikrishnan Sreekumar <hk.sreekumar@tu-braunschweig.de>
2024-08-06 13:08:54 +0200

    Linked with submodule A

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
+#include <function.h>
+
 int main() {
     std::cout << "> Hello from main module!\n";
+
+    function MyFunction;
+    MyFunction.print();
+
     return 0;
 }
